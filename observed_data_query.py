55ec6cda5ea8eb0f4848c7dddd241596cf2064dc
Gabriel Lammoc <gabrielalmeida9831@gmail.com>
2022-03-23 16:23:32 -0400

    Cancelling time condition

--- observed_data_query.py
+++ observed_data_query.py
@@ -12,17 +12,18 @@ sys.path.append("/home/lammoc/Gabriel/RNA_Operacional_WRF")
 import API as obs
 now = datetime.datetime.now()
 
-if str(now.minute) == '1':
+# commented due to application on CRON
+#if str(now.minute) == '1':
     
-    new_data = obs.api_niteroi('chuva')#.drop(' ', axis=1)
-    try:
-        old_data = pd.read_csv('./files/obs_data/hourly_data.csv')        
-        concat_data = pd.concat([old_data, new_data])
-    except:
-        print('No old data found, creating new one')
-        concat_data = new_data    
+new_data = obs.api_niteroi('chuva')#.drop(' ', axis=1)
+try:
+old_data = pd.read_csv('./files/obs_data/hourly_data.csv')        
+concat_data = pd.concat([old_data, new_data])
+except:
+print('No old data found, creating new one')
+concat_data = new_data    
 
-    concat_data = concat_data[['Lat', 'lon', 'estação', 'cidade', 'data', 'chuva 15m', 'chuva 1h', 'chuva 4h', 'chuva 24h', 'chuva 96h', 'chuva 30d', 'atual ou atrasado','fonte do dado']]
-    concat_data.to_csv('./files/obs_data/hourly_data.csv')
+concat_data = concat_data[['Lat', 'lon', 'estação', 'cidade', 'data', 'chuva 15m', 'chuva 1h', 'chuva 4h', 'chuva 24h', 'chuva 96h', 'chuva 30d', 'atual ou atrasado','fonte do dado']]
+concat_data.to_csv('./files/obs_data/hourly_data.csv')
 
-    print(now)
+print(now)
